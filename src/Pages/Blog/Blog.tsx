9f101d20f70eae8ab6d73ffe5ceaaf6c1928b881
MUIZ OWOLABI <beasteye.official@gmail.com>
2024-09-28 08:41:01 +0100

    Updated

--- src/Pages/Blog/Blog.tsx
+++ src/Pages/Blog/Blog.tsx
@@ -1,7 +1,7 @@
 const Blog = () => {
   return (
     <div className="flex justify-center place-items-center h-[100vh]">
-      <h1 className="flex text-[80px] font-bold">
+      <h1 className="flex text-[80px] h4 font-bold">
         BLOG IS UNDER
         <br /> MAINTAINANCE
       </h1>
